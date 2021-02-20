0bf8aa7aa8e5b064b7c89dcf6cef0703bf99a744
007steve <stephen.plummer1.sp@gmail.com>
2021-02-19 22:35:17 -0500

    added user route

---
+++ route/user.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+//Register
+router.post("/register", async (req, res) => {
+  res.send("ok");
+});
+//Login
+router.post("/login", async (req, res) => {
+    res.send("ok");
+  });
